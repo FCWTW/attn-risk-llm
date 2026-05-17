bcf38f4781d3e2a889e2bfb257322a0b17455303
Wayne <cweifang0626@gmail.com>
2026-05-17 15:56:33 +0800

    update 0517

--- DAM/scripts/get_heatmap.py
+++ DAM/scripts/get_heatmap.py
@@ -1,10 +1,10 @@
+import argparse
 import os
 import glob
 import cv2
 import numpy as np
-
-INPUT_DIR = '/home/wayne/Documents/Progress/SCOUT'
-OUTPUT_DIR = 'heatmap'
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from tqdm import tqdm
 
 def get_heatmap(ori_img, mask_img):
     mask_img = cv2.normalize(mask_img, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
@@ -12,45 +12,60 @@ def get_heatmap(ori_img, mask_img):
     heatmap = cv2.applyColorMap(mask_img, cv2.COLORMAP_JET)
     ori_img = ori_img.astype(np.uint8)
     overlay = cv2.addWeighted(ori_img, 0.7, heatmap, 0.3, 0)
-    overlay = cv2.resize(
-            overlay, 
-            (overlay.shape[1] * 10, overlay.shape[0] * 10),
-            interpolation=cv2.INTER_CUBIC
-        )
     return overlay
 
+def process_single_pair(mask_path, camera_dir, output_dir):
+    try:
+        folder_name = os.path.basename(os.path.dirname(mask_path))
+        file_name = os.path.basename(mask_path)
+        jpg_file_name = file_name.replace('.png', '.jpg')
+        
+        ori_path = os.path.join(camera_dir, folder_name, jpg_file_name)
+
+        if not os.path.exists(ori_path):
+            return f"Image doesn't exist: {ori_path}"
+        if not os.path.exists(mask_path):
+            return f"Image doesn't exist: {mask_path}"
+
+        mask_img = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        ori_img = cv2.imread(ori_path)
+
+        if mask_img is None or ori_img is None:
+            return f"Failed to load images..."
+
+        heatmap_img = get_heatmap(ori_img, mask_img)
+
+        output_folder = os.path.join(output_dir, folder_name)
+        os.makedirs(output_folder, exist_ok=True)
+        output_path = os.path.join(output_folder, jpg_file_name)
+        cv2.imwrite(output_path, heatmap_img)
+        
+        return None
+    except Exception as e:
+        return f"Error on {mask_path}: {str(e)}"
+
 if __name__ == "__main__":
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
-    base_image_paths = glob.glob(os.path.join(INPUT_DIR, '*.jpg'))
-    for base_path in base_image_paths:
-        base_name = os.path.splitext(os.path.basename(base_path))[0]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gazemap_dir', required=True, type=str, help='Path to gazemap folder')
+    parser.add_argument('--rgb_dir', required=True, type=str, help='Path to rgb image folder')
+    parser.add_argument('--output_dir', required=True, type=str, help='Path to output folder')
+    args = parser.parse_args()
+    
+    os.makedirs(args.output_dir, exist_ok=True)
+    search_pattern = os.path.join(args.gazemap_dir, '*', '*.png')
+    mask_paths = glob.glob(search_pattern)
+    total_files = len(mask_paths)
+    
+    print(f"A total of {total_files} mask images were found and are ready for processing...")
 
-        # Read RGB images
-        ori_img = cv2.imread(base_path, cv2.IMREAD_COLOR)
-        if ori_img is None:
-            print(f"Failed to load {base_path}...")
-            continue
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        futures = {executor.submit(process_single_pair, path, args.rgb_dir, args.output_dir): path for path in mask_paths}
+        
+        with tqdm(total=total_files, desc="Processing Images") as pbar:
+            for future in as_completed(futures):
+                result = future.result()
+                if result is not None:
+                    tqdm.write(result) 
+                pbar.update(1)
 
-        # Read all mask .png (ex: '112_00127_*.png')
-        mask_pattern = os.path.join(INPUT_DIR, f"{base_name}_*.png")
-        mask_paths = glob.glob(mask_pattern)
-
-        if not mask_paths:
-            print(f"Can't find any mask based on {base_name}...")
-            continue
-
-        # Process heatmap
-        for mask_path in mask_paths:
-            mask_img = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-            if mask_img is None:
-                print(f"Failed to load {mask_path}...")
-                continue
-
-            output = get_heatmap(ori_img, mask_img)
-            mask_basename = os.path.splitext(os.path.basename(mask_path))[0]
-            output_filename = f"{mask_basename}.jpg"
-            output_path = os.path.join(OUTPUT_DIR, output_filename)
-            cv2.imwrite(output_path, output)
-            print(f"Saved successfully: {output_path}")
-
-    print("All images processed！")
+    print("All heatmaps have been processed!")
