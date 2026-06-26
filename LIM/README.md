df8f02a5323d8a3d078cf36a7d0db10d4f713833
Wayne <cweifang0626@gmail.com>
2026-06-26 13:50:35 +0800

    update 0626

--- LIM/README.md
+++ LIM/README.md
@@ -1,8 +1,13 @@
 # Deployment details for LLM Inference Module
-```bash=
+```bash
 # In conda env python 3.10
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 pip install git+https://github.com/huggingface/transformers accelerate
 pip install "qwen-vl-utils[decord]>=0.0.14"
 pip install "vllm==0.11.0"
+```
+
+Use the following command to test your environment:
+```bash
+python3 .check.py
 ```
--- LIM/check.py
+++ LIM/check.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
             "content": [
                 {
                     "type": "image",
-                    "image": "./test.png",
+                    "image": "./sample.png",
                 },
                 {
                     "type": "text", 
@@ -86,51 +86,3 @@ if __name__ == '__main__':
     total_time = time.perf_counter() - total_start
     print(f"\nInference Time : {inference_time:.3f} seconds")
     print(f"Total Time     : {total_time:.3f} seconds")
-'''
-import torch
-from transformers import AutoModelForImageTextToText, AutoProcessor
-
-model_id = "Qwen3-VL-8B-Instruct-FP8"
-model = AutoModelForImageTextToText.from_pretrained(
-    model_id, dtype="auto", device_map="auto"
-)
-
-processor = AutoProcessor.from_pretrained(model_id)
-messages = [
-    {
-        "role": "user",
-        "content": [
-            {
-                "type": "image",
-                "image": "./test.png",
-            },
-            {
-                "type": "text", 
-                "text": """這是一張行車紀錄器拍下的危險畫面。請扮演具備數十年駕駛經驗的教練，針對此畫面生成一份簡易風險報告，內容必須包含：
-                        - [事故起因] 畫面中哪些物件或車輛處於危險狀態？為什麼這個場景很危險？
-                        - [安全建議] 駕駛當下應該如何反應（如減速、煞車、避讓）？
-                        請條列式直接回答，文字請精煉。"""
-            },
-        ],
-    }
-]
-
-inputs = processor.apply_chat_template(
-    messages,
-    tokenize=True,
-    add_generation_prompt=True,
-    return_dict=True,
-    return_tensors="pt"
-)
-inputs = inputs.to(model.device)
-
-generated_ids = model.generate(**inputs, max_new_tokens=256)
-generated_ids_trimmed = [
-    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-]
-output_text = processor.batch_decode(
-    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-)
-
-print(output_text[0])
-'''
